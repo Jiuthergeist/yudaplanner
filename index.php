c50390271cd319b8a17e4b9e1769092d3bd5f04d
Alexander Yu <projectproject711@gmail.com>
2016-11-05 02:38:18 -0700

    Created css file, tried to begin dividing page

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
 <!DOCTYPE html>
+
+<?php
+	// require_once("../includes/functions.php");
+?>
+
 <html>
+
 <head>
+
+	<title>YudaPlanner</title>
+	<link href="main.css" rel="stylesheet" type="type/css"/>
+
 </head>
+
 <body>
+	<div class="topLeft">
+		<p> hi </p>
+	</div>
+
+	<div class="topRight">
+		<p> hi </p>
+	</div>
+
+	<div class="bottomLeft">
+		<p> hi </p>
+	</div>
+
+	<div class="bottomRight">
+		<p> hi </p>
+	</div>
+
+
 </body>
+
 </html>
