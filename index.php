b2a64ab9dafec78919754a4f3b30c7d468b5e5e1
Alexander Yu <projectproject711@gmail.com>
2017-01-11 11:21:41 -0800

    Rearranged Top Column, constrained Class search bar

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
 	<head>
 
 		<title>YudaPlanner</title>
-<!-- 		<link href="main.css" type="text/css" rel="stylesheet"> -->
 
 		<!-- Import Stylesheets -->
+		<link href="main.css" type="text/css" rel="stylesheet">
 		<link href="select2-4.0.3/dist/css/select2.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
 
@@ -21,7 +21,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 		<!-- Import select2 -->
 		<script src="select2-4.0.3/dist/js/select2.min.js"></script>
-		<!-- Import Javascript -->
+		<!-- Import Bootstrap Javascript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 	</head>
@@ -30,6 +30,7 @@
 
 
 		<div class="container">
+
 			<!-- NAVBAR -->
 			<div class="navbar navbar-default" role="navigation">
 
@@ -61,16 +62,20 @@
 				<div class="col-md-4" id="topLeft">
 					<p> TOP LEFT - GENERAL STATS </p>
 				</div>
-				<div class="col-md-8" id="topRight">
+				<div class="col-md-4" id="topMiddle">
+					<p> TOP MIDDLE - MY SELECTED CLASSES </p>
+				</div>
+				<div class="col-md-4" id="topRight">
 					<p> TOP RIGHT - CLASS SELECTION </p>
 					<div id="select2-classes" class="select2-drop">
 
-						<select class="class-selection">
-						  	<option value="EE16A">EE16A</option>
-						  	<option value="EE16B">EE16B</option>
-						  	<option value="CS61A">CS61A</option>
-						  	<option value="CS61B">CS61B</option>
-						  	<option value="CS61C">CS61C</option>
+						<select id="class-menu" class="class-selection">
+						  	<option value="EE16A">EE16A: Designing Information Systems and Devices I</option>
+						  	<option value="EE16B">EE16B: Designing Information Systems and Devices II</option>
+						  	<option value="CS61A">CS61A: The Structure and Interpretation of Computer Programs</option>
+						  	<option value="CS61B">CS61B: Data Structures</option>
+						  	<option value="CS61C">CS61C: Machine Structures</option>
+						  	<option value="CS70">CS70: Discrete Mathematics and Probability Theory</option>
 						</select>
 						<script>
 							$("select").select2();
