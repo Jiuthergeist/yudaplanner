6ac3226ab16a1986bfca0f191a21a464d49024be
Alexander Yu <projectproject711@gmail.com>
2017-01-10 17:41:46 -0800

    Implemented select2 Dropdown; CSS needs to be re-written

--- index.php
+++ index.php
@@ -6,31 +6,54 @@
 
 <html>
 
-<head>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
 
-<title>YudaPlanner</title>
-<link href="main.css" rel="stylesheet" type="text/css"/>
+	<div class="container-fluid">
 
-</head>
+	<head>
 
-<body>
-	<div id="topLeft">
-		<p> hi </p>
+		<title>YudaPlanner</title>
+		<link href="select2-4.0.3/dist/css/select2.min.css" rel="stylesheet">
+
+	</head>
+
+	<body>
+		<!-- Load jQuery FIRST -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<!-- Then import select2 -->
+		<script src="select2-4.0.3/dist/js/select2.min.js"></script>
+
+		<div id="topLeft">
+			<p> hi </p>
+		</div>
+
+		<div id="topRight">
+			<p> LEL </p>
+			<div id="select2-classes" class="select2-drop">
+
+				<select class="class-selection">
+				  	<option value="EE16A">EE16A</option>
+				  	<option value="EE16B">EE16B</option>
+				  	<option value="CS61A">CS61A</option>
+				  	<option value="CS61B">CS61B</option>
+				  	<option value="CS61C">CS61C</option>
+				</select>
+				<script>
+					$("select").select2();
+				</script>
+			</div>
+		</div>
+
+		<div id="bottomLeft">
+			<p> hi </p>
+		</div>
+
+		<div id="bottomRight">
+			<p> hi </p>
+		</div>
+
+	</body>
+
 	</div>
-
-	<div id="topRight">
-		<p> hi </p>
-	</div>
-
-	<div id="bottomLeft">
-		<p> hi </p>
-	</div>
-
-	<div id="bottomRight">
-		<p> hi </p>
-	</div>
-
-
-</body>
 
 </html>
